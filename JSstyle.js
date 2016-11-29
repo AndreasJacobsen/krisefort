0c441bf69821cff3540c5d4643bc2004b789db2c
Andreas Jacobsen <andreas1300@gmail.com>
2016-11-29 22:55:51 +0100

    Calculator.js now updates all fields.

--- JSstyle.js
+++ JSstyle.js
@@ -1,7 +1,9 @@
 function ShowImage(){
     document.getElementById('loadingImage').style.display='block';
+    document.getElementById("loadText").style.display='block';
+
 }
 function HideImage(){
     document.getElementById('loadingImage').style.display='none';
-
+    document.getElementById("loadText").style.display='none';
 }
--- calculator.js
+++ calculator.js
@@ -1,18 +1,28 @@
+var printCounter = 0;
+var input;
 function calculator () {
-
     var counter = 0;
-    var id = "id";
-     for(var i=0; i<3; ++i) {
+    var negativeCounter = 0;
+    var positiveCounter =0;
+     for(var i=0; i<26; ++i) {
          var input = document.getElementById("id" + i);
-         var isChecked = input.checked;
-         if (input.checked) {
+         // var isChecked = input.checked;
+
+          if(input.checked)
+         {
              console.log("Checkbox ID " + i + " is checked");
-             counter = counter+25;
-             console.log("The counter is: " + counter);
+             // positiveCounter = 0;
+             counter = counter+3.84615384615;
+             console.log("The counter is: " + positiveCounter);
          }
-         var printCounter = counter.toString();
+          printCounter = counter;
+          printCounter = Math.ceil(printCounter);
+          // counter = positiveCounter;
          // document.querySelector('results').innerHTML = 'Percentage complete: '+printCounter;
-         document.getElementById("results").innerHTML = "The game is compatible " + counter +"% with GCAG" ;
-     }
+         document.getElementById("results").innerHTML = "The game is compatible " + printCounter +"% with GCAG" ;
+         document.getElementById("results1").innerHTML = "The game is compatible " + printCounter +"% with GCAG" ; }
+         document.getElementById("results2").innerHTML = "The game is compatible " + printCounter +"% with GCAG" ;
+         document.getElementById("results3").innerHTML = "The game is compatible " + printCounter +"% with GCAG" ;
+
 
 }
