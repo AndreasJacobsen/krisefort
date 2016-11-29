9a52e3fbe10187d96288075e6452ddec17ec30a5
Andreas Jacobsen <andreas1300@gmail.com>
2016-11-30 00:15:04 +0100

    Endret på beregniner og for-løkke da siste ikke ble beregnet.

--- calculator.js
+++ calculator.js
@@ -1,24 +1,16 @@
-var printCounter = 0;
-var input;
 function calculator () {
     var counter = 0;
-    var negativeCounter = 0;
-    var positiveCounter =0;
-     for(var i=0; i<26; ++i) {
+     for(var i=0; i<=26; ++i) {
          var input = document.getElementById("id" + i);
-         // var isChecked = input.checked;
+
 
           if(input.checked)
          {
-             console.log("Checkbox ID " + i + " is checked");
-             // positiveCounter = 0;
-             counter = counter+3.84615384615;
-             console.log("The counter is: " + positiveCounter);
+             counter = counter+3.7037037037;
          }
-          printCounter = counter;
+          var printCounter = counter;
           printCounter = Math.ceil(printCounter);
-          // counter = positiveCounter;
-         // document.querySelector('results').innerHTML = 'Percentage complete: '+printCounter;
+
          document.getElementById("results").innerHTML = "The game is compatible " + printCounter +"% with GCAG" ;
          document.getElementById("results1").innerHTML = "The game is compatible " + printCounter +"% with GCAG" ; }
          document.getElementById("results2").innerHTML = "The game is compatible " + printCounter +"% with GCAG" ;
