43d8493918e3496a555e008deea5e32823375699
Andreas Jacobsen <andreas1300@gmail.com>
2016-11-30 19:02:28 +0100

    Æ like ikke gamle sokka i grøden min

--- JSstyle.js
+++ JSstyle.js
@@ -1,7 +1,10 @@
+var guntext = "Here we see a weapon displayed in menu in a game. <br>" +
+    "How rare the waepon is is displayed ony using the color purple, normal weapons are blue. "
+
 function ShowImage(){
+    document.getElementById("guntext").innerHTML = "ostekake";
     document.getElementById('loadingImage').style.display='block';
     document.getElementById("loadText").style.display='block';
-    document.getElementById("guntext").innerHTML = "ostekake";
 
 
 }
--- calculator.js
+++ calculator.js
@@ -2,14 +2,14 @@ function calculator () {
 
     var counter = 0;
     //the for-loop has hard coded number of checkboxes, this can be solved in other more automated ways. We see the GCAG rules as rather static so we did not implemented with an auto-counter.
-     for(var i=0; i<=26; ++i) {
+     for(var i=0; i<=27; ++i) {
          //IDs are numbered from 0-26, the for-loop appends id numbers matching the ID-numbers in the form.
          var input = document.getElementById("id" + i);
 
          //checks if counter is checked, here we also hardcoded the value (sum of checkboxes divided by 100%), this could of course be done in a more automated way; but as the rules are rather static we didn't spend any time on that.
           if(input.checked)
          {
-             counter = counter+3.7037037037;
+             counter = counter+3.57;
          }
           var printCounter = counter;
           printCounter = Math.ceil(printCounter);
